01081b8dc6b5350973b91660dfc82a80c07a209b
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2023-12-04 23:04:53 +0700

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Yong on 4/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
